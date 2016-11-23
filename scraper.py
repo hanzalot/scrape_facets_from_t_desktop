dc8a0a1bd1e6f4d1a4132af4120457287f7d9c0a
Hanz Rauch <hanz.rauch@takealot.com>
2016-11-23 11:56:25 +0200

    Get toplevel
    See if I can get the toplevel nodes in the mega menu

--- scraper.py
+++ scraper.py
@@ -1,24 +1,35 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
+# combining from https://github.com/wfdd/inatsisartut-scraper/blob/master/scraper.py 
+# AND https://github.com/ianibo/SirsiDynixIBistroScraper/blob/master/scraper.py
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import lxml.html
+import hashlib
+import re
+from splinter import Browser
+import sys, traceback, logging, shutil, platform
+from string import ascii_lowercase
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+PARSER_ID = 'hanz'
+
+base_url = 'http://www.takealot.com'
+
+
+def gather_landing_pages(session):
+  for toplevel in session.find_by_xpath('//*[@id="nav"]/li/ul/li'):
+    name = toplevel.find_by_xpath('a/span[1]')
+    url = toplevel.find_by_xpath('a')['href']
+    print name + ',' + url
+
+
+def main():
+    with Browser('phantomjs', load_images=False) as browser:
+        browser.visit(base_url)
+        landing_pages = list(gather_landing_pages(browser))
+        
+        # a bunch of stuff excluded for now
+        
+  
+if __name__ == '__main__':
+    main()
